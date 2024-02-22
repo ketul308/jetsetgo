38ef5802294b3b6a9764bc829b4c0c87db3f1d9e
Ketul Patel <patelketul308@gmail.com>
2024-02-22 15:29:33 +0530

    minor changes

--- src/Navigation/RootNavigation.tsx
+++ src/Navigation/RootNavigation.tsx
@@ -7,7 +7,10 @@ import SplashScreen from 'react-native-splash-screen';
 export default function RootNavigation() {
 
     React.useEffect(() => {
-        SplashScreen.hide();
+        const _timeOut = setTimeout(() => {
+            SplashScreen.hide();
+            clearTimeout(_timeOut);
+        }, 3000);
     }, []);
 
     return (
